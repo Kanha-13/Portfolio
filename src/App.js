fb454141ea2fe50dab081a8572e57ec4c2eb5ea1
Kanha Agrawal <kanha.agr11@gmail.com>
2022-02-01 14:18:43 +0530

    repel water effect-background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
+import './Css/app.css'
+import './Css/repel.css'
 const App = () => {
+  const [startRepel, setStartRepel] = useState(false)
+  const [hideDiv, setHideDiv] = useState(false)
+  const [opacity, setOpacity] = useState(1)
+  const repel = () => {
+    setStartRepel(true)
+    setOpacity(0)
+    setTimeout(() => {
+      setHideDiv(true)
+    }, 5000);
+  }
   return (
-    <div>
-      <h1>hello</h1>
+    <div style={{ opacity: opacity, display: hideDiv ? "none" : "flex  " }} className='container'>
+      <h1 style={{ opacity: opacity }} onClick={repel} className='hello'>hello world</h1>
+      {
+        startRepel &&
+        <>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </>
+      }
+      {/* <h1 className='name' style={{ display: !show ? "flex" : "none", alignSelf: "center" }}>My name is Kanha Agrawal</h1> */}
+      {/* <h1 className='begin' style={{ display: !show ? "flex" : "none" }}>Let's begin</h1> */}
     </div>
   );
 }
