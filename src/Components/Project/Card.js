bc35d9db99856af531486bc39a547a3420586f28
Kanha Agrawal <kanha.agr11@gmail.com>
2022-07-15 18:48:52 +0530

    fix screen size ui bug

--- src/Components/Project/Card.js
+++ src/Components/Project/Card.js
@@ -1,8 +1,7 @@
 const ProjectCard = ({ details }) => {
   return (
-    <div style={{ cursor: "pointer", border: "3px solid gray", backgroundColor: "#ffffff", borderRadius: "10px", overflow: "hidden", height: "30vh", width: "20vw" }}>
-      {/* <p style={{ position: "absolute", zIndex: 2 }}>{details.projectName}</p> */}
-      <img src={details.image} style={{ zIndex: 1, width: "20vw", borderRadius: "10px", height: "30vh", backgroundSize: "cover", position: "absolute" }} />
+    <div className="project-card">
+      <img src={details.image} />
     </div>
   );
 }
